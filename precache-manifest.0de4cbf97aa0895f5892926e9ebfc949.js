8c471ed3080a765b05d4b43481006d2941d2111c
Simon X <simonxiong11@gmail.com>
2018-12-16 17:09:10 -0800

    Updates

---
+++ precache-manifest.0de4cbf97aa0895f5892926e9ebfc949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fe3c286524cc5894d260",
+    "url": "/tictactoe-react/static/js/runtime~main.fe3c2865.js"
+  },
+  {
+    "revision": "a036ffc5ce337476cab5",
+    "url": "/tictactoe-react/static/js/main.a036ffc5.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/tictactoe-react/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "a036ffc5ce337476cab5",
+    "url": "/tictactoe-react/static/css/main.fb5d9969.chunk.css"
+  },
+  {
+    "revision": "079c3e1b2bf7ce2658055f656943a465",
+    "url": "/tictactoe-react/index.html"
+  }
+];
